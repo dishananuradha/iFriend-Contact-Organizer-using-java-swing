0623bb7d28b2335f26fbe40b3d83c41cfd35aa2c
Dishan Anuradha <dishananuradha@gmail.com>
2024-06-19 01:52:56 +0530

    MainWindow.java created

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main{
 	public static void main(String[] args){
-		new MainWindow().setvisible(true);
+		new MainWindow().setVisible(true);
 	}
 }
--- MainWindow.java
+++ MainWindow.java
@@ -1,31 +1,78 @@
-/*
- * MainWindow.java
- * 
- * Copyright 2024 Springboard <Springboard@I7LAPTOP>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
+import java.awt.*;
+import javax.swing.*;
 
-
-public class untitled {
+class MainWindow extends JFrame{
+	private JButton btnAddContact;
+	private JButton btnUpdateContact;
+	private JButton btnSearchContact;
+	private JButton btnDeleteContact;
+	private JButton btnViewContacts;
+	private JButton btnExit;
 	
-	public static void main (String[] args) {
-		
+	public MainWindow(){
+		setTitle("iFriend Contact Manager");
+		setSize(500, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+
+		JLabel lblTitle = new JLabel("iFriend Contact Manager");
+		lblTitle.setFont(new Font("Arial", Font.BOLD, 24));
+		lblTitle.setForeground(Color.BLUE);
+		lblTitle.setHorizontalAlignment(JLabel.CENTER);
+		add(lblTitle, BorderLayout.WEST);
+
+		JPanel panel1 = new JPanel(new BorderLayout());
+
+		JLabel lblHome = new JLabel("Home Page");
+		lblHome.setFont(new Font("Arial", Font.BOLD, 18));
+		lblHome.setForeground(Color.BLUE);
+		lblHome.setHorizontalAlignment(JLabel.CENTER);
+		panel1.add(lblHome, BorderLayout.NORTH);
+
+		JPanel panel2 = new JPanel(new GridLayout(5, 1, 5, 5));
+
+		btnAddContact = new JButton("Add New Contact");
+		btnAddContact.setFont(new Font("Arial", Font.BOLD, 14));
+		btnAddContact.setForeground(Color.BLUE);
+		btnAddContact.setHorizontalAlignment(JButton.CENTER);
+		panel2.add(btnAddContact);
+
+		btnUpdateContact = new JButton("Update Contact");
+		btnUpdateContact.setFont(new Font("Arial", Font.BOLD, 14));
+		btnUpdateContact.setForeground(Color.BLUE);
+		btnUpdateContact.setHorizontalAlignment(JButton.CENTER);
+		panel2.add(btnUpdateContact);
+
+		btnSearchContact = new JButton("Search Contact");
+		btnSearchContact.setFont(new Font("Arial", Font.BOLD, 14));
+		btnSearchContact.setForeground(Color.BLUE);
+		btnSearchContact.setHorizontalAlignment(JButton.CENTER);
+		panel2.add(btnSearchContact);
+
+		btnDeleteContact = new JButton("Delete Contact");
+		btnDeleteContact.setFont(new Font("Arial", Font.BOLD, 14));
+		btnDeleteContact.setForeground(Color.BLUE);
+		btnDeleteContact.setHorizontalAlignment(JButton.CENTER);
+		panel2.add(btnDeleteContact);
+
+		btnViewContacts = new JButton("View Contacts");
+		btnViewContacts.setFont(new Font("Arial", Font.BOLD, 14));
+		btnViewContacts.setForeground(Color.BLUE);
+		btnViewContacts.setHorizontalAlignment(JButton.CENTER);
+		panel2.add(btnViewContacts);
+
+		panel1.add(panel2, BorderLayout.CENTER);
+
+		JPanel panel3 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+		btnExit = new JButton("Exit");
+		btnExit.setFont(new Font("Arial", Font.BOLD, 14));
+		btnExit.setForeground(Color.BLUE);
+		btnExit.setHorizontalAlignment(JButton.CENTER);
+		panel3.add(btnExit);
+
+		panel1.add(panel3, BorderLayout.SOUTH);
+
+		add(panel1, BorderLayout.EAST);
 	}
 }
-
